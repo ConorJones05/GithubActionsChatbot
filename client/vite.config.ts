bc357314df6e580bb4a9f42f2ef90d7d61a9a230
Conor Jones <155279919+ConorJones05@users.noreply.github.com>
2025-04-17 00:03:14 -0400

    added ./ base

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   css: {
     postcss: {
